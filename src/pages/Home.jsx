abfff36094fc040f3f24761142abfc016c2771f8
(YunJiSung) <(capitaljs@gmail.com)>
2023-11-17 17:00:16 +0900

    v

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import { travelerText } from '../data/traveler'
-import { Link } from 'react-router-dom'
-import { todayText } from '../data/today'
 import Today from '../components/contents/Today'
 import Traveler from '../components/contents/Traveler'
 
